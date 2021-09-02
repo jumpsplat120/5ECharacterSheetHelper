a960c53700e539630ce5af1bf32c31c25b2ee164
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 18:59:22 -0700

    Changed how revert/obvert works

--- bin/Stat.lua
+++ bin/Stat.lua
@@ -24,11 +24,13 @@ function Stat:new(value, reason)
     private[self.uuid].value = value
 
     private[self.uuid].history = { createEntry(value, "INIT", reason) }
-    private[self.uuid].index   = 1
+    private[self.uuid].index   = 0
 end
 
 function Stat:get_value() return private[self.uuid].value end
-function Stat:set_value(val) error("Use the 'change' method to update a stat.") end
+function Stat:get_history() return private[self.uuid].history end
+function Stat:set_value(_) error("Use the 'change' method to update a stat.") end
+function Stat:set_history(_) error("You may not change the history table directly.") end
 
 function Stat:change(val, action, reason)
     local p       = private[self.uuid]
@@ -38,30 +40,39 @@ function Stat:change(val, action, reason)
     
     p.value = val
 
-    p.history[#p.history + 1] = createEntry(val, action, reason)
     p.index = p.index + 1
+    p.history[p.index] = createEntry(val, action, reason)
+    while p.index != #p.history do p.history[#p.history] = nil end
 end
 
 function Stat:revert(steps, reason)
     local p  = private[self.uuid]
-    local ni = math.max(math.min(p.index - (steps or 1), 1), #p.history - 1)
     
-    assert(not p.obvert_entry, "Unable to revert as you've already reverted.")
+    steps = steps or 1
 
-    p.obvert_entry = p.history[#p.history].value
-    p.index = ni
+    assert(p.index == 1, "Unable to revert as you as there is nothing to revert to.")
+    assert(type(steps) == "number", "You can only revert a numeric amount.")
+    assert(steps < 1, "You can only revert a positive number of steps. Use obvert if you'd like to 'redo'.")
+    assert(p.index - steps > 0, "You can only revert to the first entry. (Tried to revert " .. steps .. " times, there are only " .. #p.history .. " entries.)")
 
-    self:change(p.history[ni].value, "REVERT", reason or "No reason given.")
+    p.index = p.index - steps
+
+    self:change(p.history[steps].value, "REVERT", reason or "No reason given.")
 end
 
-function Stat:obvert(reason)
+function Stat:obvert(steps, reason)
     local p = private[self.uuid]
 
-    assert(p.obvert_entry, "Unable to obvert as you have not reverted.")
+    steps = steps or 1
+
+    assert(p.index == #p.history, "Unable to obvert as you as there is nothing to obvert to.")
+    assert(type(steps) == "number", "You can only obvert a numeric amount.")
+    assert(steps < 1, "You can only obvert a positive number of steps. Use revert if you'd like to 'undo'.")
+    assert(p.index + steps > #p.history, "You can only obvert to the last entry. (Tried to obvert to the " .. steps .. " entry, there are only " .. #p.history .. " entries.)")
+
+    p.index = p.index + steps
 
     self:change(p.obvert_entry, "OBVERT", reason or "No reason given.")
-
-    p.obvert_entry = nil
 end
 
 return Stat
